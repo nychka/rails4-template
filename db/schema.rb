163ce7848b7c94c2ae0d4e94d321cb36e60208ef
nychos <nychka08@yandex.ru>
2014-05-10 18:26:56 +0300

    integrated bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140510144026) do
+ActiveRecord::Schema.define(version: 20140510150205) do
 
   create_table "users", force: true do |t|
     t.string   "name"
